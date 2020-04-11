97059fbd8b16bf5ba3ce399378b006fa69c197fa
thealpha <theamanamanaman@gmail.com>
2020-04-12 03:56:39 +0530

    Create counter_gamebit.cpp

---
+++ counter_gamebit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   vector<int> ss{12,34,234,2};
+
+    s.sort(s.begin(),s.end());
+    for(int i=0; i<s.size() ; i++)
+    {  cout<<s[i];
+
+
+    }
+    return 0;
+}
