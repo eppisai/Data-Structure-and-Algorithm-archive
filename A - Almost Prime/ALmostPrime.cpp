9fa19bc4e4596590bcfeecb56e00f6e96c301f43
Aman <54789531+the-eppisai@users.noreply.github.com>
2020-06-26 01:55:13 +0530

    Update ALmostPrime.cpp

--- A - Almost Prime/ALmostPrime.cpp
+++ A - Almost Prime/ALmostPrime.cpp
@@ -1,35 +1,31 @@
-include<bits / stdc++.h> using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-#define endl "\n"
-#define sd(val) scanf("%d", &val)
-#define ss(val) scanf("%s", &val)
-#define sl(val) scanf("%lld", &val)
-#define debug(val) printf("check%d\n", val)
-#define all(v) v.begin(), v.end()
-#define PB push_back
-#define MP make_pair
-#define FF first
-#define SS second
-#define ll long long
-#define MOD 1000000007
-#define clr(val) memset(val, 0, sizeof(val))
-#define what_is(x) cerr << #x << " is " << x << endl;
-#define OJ                            \
-    freopen("input.txt", "r", stdin); \
-    freopen("output.txt", "w", stdout);
-#define FIO                           \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
+void prime(int* p){
+    for(int i = 2; i<3005; i++){
+        if(p[i] == 0){
+            
+            for(int j = i*2; j<3005; j+=i){ p[j]++;
+                if(j == 2) break;
+            }
+            
+        }
+    }
+}
 
+
+ 
 int main()
 {
-    OJ;
-    int a;
-    cin >> a;
-    cout << a;
-
-    return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+ 
+   int n,k = 0;
+   cin>>n;
+   int p[3005] = {0};
+   prime(p);
+   for(int i = 0; i<=n; i++) if(p[i] == 2) k++;
+   cout<<k;
+    return 0 ;
+ 
 }
