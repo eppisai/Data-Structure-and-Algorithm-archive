3fd725f25f9509e39e1bed51c1d92e0fff6b0737
thealpha <theamanamanaman@gmail.com>
2020-07-03 12:43:25 +0530

    soving pair if topics codeforces

---
+++ Codeforces/D - Pair Of Topics/Pair Of Topics(PBDS) codeforces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	return 0;
+}
