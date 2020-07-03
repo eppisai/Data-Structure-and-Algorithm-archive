1d8138b2202bfed58783b884ec8ea85ace304cb2
thealpha <theamanamanaman@gmail.com>
2020-07-04 05:05:58 +0530

    Update Pair Of Topics(PBDS) codeforces.cpp

--- Codeforces/D - Pair Of Topics/Pair Of Topics(PBDS) codeforces.cpp
+++ Codeforces/D - Pair Of Topics/Pair Of Topics(PBDS) codeforces.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 int main()
 {
@@ -10,6 +17,30 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+
+	int n;
+	cin >> n;
+	int a[n], b[n], c[n];
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+
+	for (int i = 0; i < n; i++)
+		c[i] = a[i] - b[i];
+
+	pbds t;
+	long long int ans = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		ans += t.size() - t.order_of_key({-c[i], 1000000});
+		t.insert({c[i], i});
+	}
+
+	cout << ans;
 
 	return 0;
 }
