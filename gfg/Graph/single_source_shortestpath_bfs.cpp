55accb753433abbb4c50e88cb7420530c9ecbcb2
Aman <theamanamanaman@gmail.com>
2020-12-17 09:05:03 +0530

    sssp graph bfs

--- gfg/Graph/single_source_shortestpath_bfs.cpp
+++ gfg/Graph/single_source_shortestpath_bfs.cpp
@@ -18,18 +18,27 @@ class Graph{
         map<T, int> dist;
         queue<T> q;
 
+        //all other nodes will have int_max
+        for(auto node_pair:l){
+            T node = node_pair.first;
+            dist[node] = INT_MAX;
+            
+        }
+
         q.push(src);
-        visited[src] = true;
+        dist[src] = 0;
+
+        
 
         while(!q.empty()){
             T node = q.front();
             q.pop();
 
             for(int nbr:l[node]){
-                if(!visited[nbr]){
+                if(dist[nbr] == INT_MAX){
                     q.push(nbr);
                     //mark that nbr as visited
-                    visited[nbr] = true;
+                    dist[nbr] = dist[node] + 1;
                 }
             }
         }
